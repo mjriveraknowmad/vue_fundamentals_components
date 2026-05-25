f654de7ec0254f1cc66f09b350f4612b085a504c
Marcos Rivera <mjrivera@knowmadmood.com>
2026-05-25 09:47:39 +0200

    feat: add AddContact and Contact components

---
+++ src/components/AddContact.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container border rounded p-2">
+    <div class="text-center text-white">Add New Contact</div>
+    <form @submit.prevent="addContact">
+      <div class="row">
+        <div class="col-4">
+          <input type="text" class="form-control" v-model="contact.name" placeholder="Name" />
+        </div>
+        <div class="col-4">
+          <input type="text" class="form-control" v-model="contact.email" placeholder="Email" />
+        </div>
+        <div class="col-4">
+          <input type="text" class="form-control" v-model.number="contact.phone" placeholder="Phone" />
+        </div>
+        <div class="col-6 offset-3 p-2">
+          <button type="submit" class="btn btn-secondary w-100">
+            Add Contact
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { reactive, defineEmits } from "vue";
+
+const emit = defineEmits(["add-contact"]);
+const contact = reactive({
+  name: "",
+  phone: "",
+  email: "",
+});
+
+function addContact() {
+  emit("add-contact", {
+    name: contact.name,
+    phone: contact.phone,
+    email: contact.email,
+  });
+  console.log(contact);
+  contact.email = "";
+  contact.name = "";
+  contact.phone = "";
+}
+</script>
---
+++ src/components/Contact.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="bg-info rounded p-1 pb-4 m-1">
+    <div class="row">
+      <div class="col-6">
+        <h3>Name :{{ name }}</h3>
+        <p>Email : {{ email }}</p>
+        <p>Phone : {{ phone }}</p>
+      </div>
+      <div class="col-3">
+        <button
+          @click="
+            emit('update-favorite', {
+              isFavorite: props.isFavorite,
+              name: props.name,
+            })
+          "
+          :class="[isFavorite ? 'btn btn-warning' : 'btn btn-success']"
+        >
+          {{ isFavorite ? "Remove from" : "Add to" }} Favorite
+        </button>
+      </div>
+    </div>
+    <span class="float-end small" v-if="ownername != ''">
+      *this contact info belongs to {{ ownername }}
+    </span>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  name: { type: String, required: true },
+  phone: Number,
+  ownername: String,
+  email: { type: String, required: false, default: "-n/a-" },
+  isFavorite: Boolean,
+});
+
+const emit = defineEmits(["update-favorite"]);
+</script>
