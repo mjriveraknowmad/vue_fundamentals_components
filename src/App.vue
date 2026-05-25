164fc823f20679eec168f2a43dedca1f45a7ec60
Marcos Rivera <mjrivera@knowmadmood.com>
2026-05-25 09:55:59 +0200

    feat: add AddContact and Contact components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,60 @@
 <template>
-  Fundamentals Components en Vue
+  <div class="bg-black text pt-3" style="min-height: 100vh">
+    <div class="container">
+      <div class="text-white float-end">
+        Contact Owner Name : <input v-model="ownerName" />
+      </div>
+      <br /><br />
+      <AddContact @add-contact="onAddContact"></AddContact>
+      <div class="row">
+        <div class="col-12" v-for="contact in contacts" :key="contact.name">
+          <Contact :name="contact.name" :phone="contact.phone" :ownername="contact.ownerName" :email="contact.email"
+            :isFavorite="contact.isFavorite" @update-favorite="
+              contact.isFavorite = onUpdateFavorite($event, contact.phone)
+              "></Contact>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { ref, reactive } from "vue";
+import Contact from "./components/Contact.vue";
+import AddContact from "./components/AddContact.vue";
+const ownerName = ref("dotnetmastery");
+const contacts = reactive([
+  {
+    name: "Bhrugen",
+    phone: 123131231,
+    ownerName: ownerName,
+    isFavorite: false,
+  },
+  {
+    name: "Bella",
+    phone: 5554443333,
+    ownerName: ownerName,
+    isFavorite: true,
+  },
+  {
+    name: "Ben",
+    phone: 33344455543,
+    ownerName: "",
+    email: "ben@dotnetmastery.com",
+    isFavorite: false,
+  },
+]);
 
+function onAddContact(contact) {
+  contact.ownerName = ownerName.value;
+  contact.isFavorite = false;
+  contacts.push(contact);
+}
+
+function onUpdateFavorite(oldValuesFromChildComponent, phoneNumberFromParent) {
+  console.log(oldValuesFromChildComponent);
+  return !oldValuesFromChildComponent.isFavorite;
+}
 </script>
 
 <style></style>
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-
-const app = createApp(App);
-//global components
-
-app.mount("#app");
